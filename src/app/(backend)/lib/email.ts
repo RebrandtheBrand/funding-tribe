28cb59a5cc8db3d6ad103fb5fc9b76027e88b7bf
Olly-E <akinsnuel@gmail.com>
2026-03-14 10:41:44 +0100

    fix api leak

--- src/app/(backend)/lib/email.ts
+++ src/app/(backend)/lib/email.ts
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-const resend = new Resend("re_BB4xa7g2_GkTB5hay5DQ4XAApKqVmDZaZ");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 interface ContactEmailParams {
   name: string;
